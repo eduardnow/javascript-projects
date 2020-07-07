265fc81119348b7be71580f5690eaa0b9dfc9e51
Eduard Castro <eduard.cas07@gmail.com>
2020-07-06 21:02:28 -0500

    read pie data from file

--- index.js
+++ index.js
@@ -1,24 +1,22 @@
 // Bring in the express server and create application
 let express = require('express');
 let app = express();
-let pieRepo = require('./repos/pie-repo');
+let pieRepo = require('./repos/pieRepo');
 
 // Use the express Router object
 let router = express.Router();
 
-let pies = [
-    { 'id': 1, 'name': 'Apple' },
-    { 'id': 2, 'name': 'Cherry' },
-    { 'id': 3, 'name': 'Peach' },
-];
-
 // Create GET to return a list of all pies
 router.get('/', function(req, res, next) {
-    res.status(200).json({
-        'status': 200,
-        'statusText': 'OK',
-        'message': 'All pies retrieved',
-        'data': pies
+    pieRepo.get(function(data) {
+        res.status(200).json({
+            'status': 200,
+            'statusText': 'OK',
+            'message': 'All pies retrieved',
+            'data': data
+        });
+    }, function(err) {
+        next(err);
     });
 });
 
