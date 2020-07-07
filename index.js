6be50f220422126cca6e815a7c051d4c1f9bd544
Eduard Castro <eduard.cas07@gmail.com>
2020-07-06 21:10:49 -0500

    Get a single piece of data

--- index.js
+++ index.js
@@ -20,6 +20,32 @@ router.get('/', function(req, res, next) {
     });
 });
 
+// Create GET to return a list of all pies
+router.get('/:id', function(req, res, next) {
+    pieRepo.getById(req.params.id, function(data) {
+        if(data) {
+            res.status(200).json({
+                'status': 200,
+                'statusText': 'OK',
+                'message': 'Single pies retrieved',
+                'data': data
+            });
+        }
+
+        res.status(404).json({
+            'status': 404,
+            'statusText': 'Not Found',
+            'message': 'The pie ' + req.params.id + ' could not be found',
+            'error': {
+                'code': 'NOT_FOUND',
+                'message': 'The pie ' + req.params.id + ' could not be found',
+            }
+        });
+    }, function(err) {
+        next(err);
+    });
+});
+
 // Configure router so all routes are prefixed with /api/v1
 app.use('/api/', router);
 
