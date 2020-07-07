3970fe24af6ee178810f9d70afe1af8d558bb1d1
Eduard Castro <eduard.cas07@gmail.com>
2020-07-07 11:23:03 -0500

    Add exception middleware

--- index.js
+++ index.js
@@ -169,6 +169,19 @@ router.post('/', function (req, res, next) {
 // Configure router so all routes are prefixed with /api/v1
 app.use('/api/', router);
 
+// Configure exception middleware last
+app.use(function(err, req, res, next) {
+    res.status(500).json({
+        'status': 500,
+        'statusText': 'Internal Server Error',
+        'message': err.message,
+        'error': {
+            'code': 'INTERNAL_SERVER_ERROR',
+            'message': err.message
+        }
+    });
+});
+
 // Create server to listen on port 5000
 var server = app.listen(5000, function () {
     console.log('Node server is running on http://localhost:5000');
