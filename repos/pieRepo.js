a38990377b0f103c4d95623d3d0e8aa9a131d2a6
Eduard Castro <eduard.cas07@gmail.com>
2020-07-07 11:49:32 -0500

    Add addtional error handlers

--- repos/pieRepo.js
+++ repos/pieRepo.js
@@ -6,9 +6,9 @@ let pieRepo = {
         fs.readFile(FILE_NAME, function (err, data) {
             if (err) {
                 reject(err);
+            } else {
+                resolve(JSON.parse(data))
             }
-
-            resolve(JSON.parse(data))
         });
     },
 
@@ -16,58 +16,44 @@ let pieRepo = {
         fs.readFile(FILE_NAME, function (err, data) {
             if (err) {
                 reject(err);
+            } else {
+                let pie = JSON.parse(data).find(p => p.id == id)
+                resolve(pie);
             }
-
-            let pie = JSON.parse(data).find(p => p.id == id)
-            resolve(pie);
         });
     },
 
     search: function (searchObject, resolve, reject) {
         fs.readFile(FILE_NAME, function (err, data) {
-            if (err) { reject(err); }
+            if (err) {
+                reject(err);
+            } else {
+                let pies = JSON.parse(data);
+                if (searchObject) {
+                    // Example search object
+                    // let searchObject = {
+                    //    'id': 1,
+                    //    'name': 'A'
+                    // }
 
-            let pies = JSON.parse(data);
-            if (searchObject) {
-                // Example search object
-                // let searchObject = {
-                //    'id': 1,
-                //    'name': 'A'
-                // }
+                    pies = pies.filter(
+                        p => (searchObject.id ? p.id == searchObject.id : true) &&
+                            (searchObject.name ? p.name.toLowerCase().indexOf(searchObject.name.toLowerCase()) >= 0 : true)
+                    );
+                }
 
-                pies = pies.filter(
-                    p => (searchObject.id ? p.id == searchObject.id : true) &&
-                        (searchObject.name ? p.name.toLowerCase().indexOf(searchObject.name.toLowerCase()) >= 0 : true)
-                );
+                resolve(pies);
             }
-
-            resolve(pies);
         });
     },
 
     insert: function (newData, resolve, reject) {
         fs.readFile(FILE_NAME, function (err, data) {
-            if (err) { reject(err); }
-
-            let pies = JSON.parse(data);
-            pies.push(newData);
-            fs.writeFile(FILE_NAME, JSON.stringify(pies, null, 4), function (err) {
-                if (err) { reject(err); }
-
-                resolve(newData);
-            });
-        });
-    },
-
-    update: function (newData, id, resolve, reject) {
-        fs.readFile(FILE_NAME, function (err, data) {
-            if (err) { reject(err); }
-
-            let pies = JSON.parse(data);
-            let pie = pies.find(p => p.id = id);
-            console.log(pie);
-            if (pie) {
-                Object.assign(pie, newData);
+            if (err) {
+                reject(err);
+            } else {
+                let pies = JSON.parse(data);
+                pies.push(newData);
                 fs.writeFile(FILE_NAME, JSON.stringify(pies, null, 4), function (err) {
                     if (err) { reject(err); }
 
@@ -77,20 +63,42 @@ let pieRepo = {
         });
     },
 
+    update: function (newData, id, resolve, reject) {
+        fs.readFile(FILE_NAME, function (err, data) {
+            if (err) {
+                reject(err);
+            } else {
+                let pies = JSON.parse(data);
+                let pie = pies.find(p => p.id = id);
+                console.log(pie);
+                if (pie) {
+                    Object.assign(pie, newData);
+                    fs.writeFile(FILE_NAME, JSON.stringify(pies, null, 4), function (err) {
+                        if (err) { reject(err); }
+
+                        resolve(newData);
+                    });
+                }
+            }
+        });
+    },
+
     delete: function (id, resolve, reject) {
         fs.readFile(FILE_NAME, function (err, data) {
-            if (err) { reject(err); }
+            if (err) {
+                reject(err);
+            } else {
+                let pies = JSON.parse(data);
+                let index = pies.findIndex(p => p.id == id);
 
-            let pies = JSON.parse(data);
-            let index = pies.findIndex(p => p.id == id);
+                if (index != -1) {
+                    pies.splice(index, 1);
+                    fs.writeFile(FILE_NAME, JSON.stringify(pies, null, 4), function (err, data) {
+                        if (err) { reject(err); }
 
-            if (index != -1) {
-                pies.splice(index, 1);
-                fs.writeFile(FILE_NAME, JSON.stringify(pies, null, 4), function (err, data) {
-                    if (err) { reject(err); }
-
-                    resolve(index);
-                });
+                        resolve(index);
+                    });
+                }
             }
         });
     }
